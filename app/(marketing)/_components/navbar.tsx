67e2735dc3ce6fd53f96f212cd481c51bd951209
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-11-08 14:56:51 -0600

    Implementing auth in some components using convex and clerk. Added spinner component. And a new layout for main routes.

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
 import Link from 'next/link';
 
 import { useScrollTop } from '@/hooks/use-scroll-top';
@@ -8,8 +10,10 @@ import { cn } from '@/lib/utils';
 import ModeToggle from '@/components/mode-toggle';
 
 import { Logo } from './logo';
+import { Spinner } from '@/components/spinner';
 
 export default function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -21,6 +25,27 @@ export default function Navbar() {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
