29373c4a527f7af263275b4979ac2b0909000fd4
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-11-14 15:49:57 -0600

    Adding new component such as banner, menu, navbar, title where some of them has certain functionalities like remove, restore, update or get a document. Added update convex mutation.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -28,6 +28,7 @@ import Item from './item';
 import TrashBox from './trash-box';
 import { useSearch } from '@/hooks/use-search';
 import { useSettings } from '@/hooks/use-settings';
+import Navbar from './navbar';
 
 export default function Navigation() {
   const router = useRouter();
@@ -214,12 +215,18 @@ export default function Navigation() {
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
+          {!!params.documentId ? (
+            <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+          ) : (
+            <nav className="bg-transparent px-3 py-2 w-full">
+              {isCollapsed && (
+                <MenuIcon
+                  onClick={resetWidth}
+                  role="button"
+                  className="h-6 w-6 text-muted-foreground"
+                />
+              )}
+            </nav>
           )}
         </nav>
       </div>
